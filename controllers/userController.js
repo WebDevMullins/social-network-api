d3045c71a7b0bfa252634af2650ab399bf622c4d
WebDevMullins <webdevmullins@gmail.com>
2023-12-06 14:33:45 -0600

    add routes and userController

---
+++ controllers/userController.js
@@ -0,0 +1,42 @@
+const { User } = require('../models')
+
+const userController = {
+	// GET all users
+	async getUsers(req, res) {
+		const userData = await User.find().select('-__v')
+		try {
+			res.json(userData)
+		} catch (err) {
+			console.error(err)
+			res.status(500).json(err)
+		}
+	},
+
+	// GET a single user by its _id
+	async getUserById(req, res) {
+		try {
+			const user = await User.findOne({ _id: req.params.id }).select('-__v')
+			console.log(req.params.id)
+			if (!user) {
+				return res.status(404).json({ message: 'No user found with this id!' })
+			}
+			res.json(user)
+		} catch (err) {
+			console.error(err)
+			res.status(400).json(err)
+		}
+	},
+
+	// Create user
+	async createUser(req, res) {
+		try {
+			const user = await User.create(req.body)
+			return res.status(200).json(user)
+		} catch (err) {
+			console.log(err)
+			return res.status(500).json(err)
+		}
+	}
+}
+
+module.exports = userController
---
+++ routes/api/userRoutes.js
@@ -0,0 +1,8 @@
+const router = require('express').Router()
+const { getUsers, createUser, getUserById } = require('../../controllers/userController')
+
+router.route('/').get(getUsers).post(createUser)
+
+router.route('/:id').get(getUserById)
+
+module.exports = router

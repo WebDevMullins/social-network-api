02b949cd8da7fdc06e989fd0bc09e28002edda25
WebDevMullins <webdevmullins@gmail.com>
2023-12-07 12:34:01 -0600

    add addFriend

--- controllers/userController.js
+++ controllers/userController.js
@@ -3,9 +3,9 @@ const { User } = require('../models')
 const userController = {
 	// GET all users
 	async getUsers(req, res) {
-		const userData = await User.find().select('-__v')
+		const users = await User.find().select('-__v')
 		try {
-			res.json(userData)
+			return res.status(200).json(users)
 		} catch (err) {
 			console.error(err)
 			res.status(500).json(err)
@@ -15,11 +15,11 @@ const userController = {
 	// GET a single user by its _id
 	async getUserById(req, res) {
 		try {
-			const user = await User.findOne({ _id: req.params.id }).select('-__v')
+			const user = await User.findOne({ _id: req.params.userId }).select('-__v')
 			if (!user) {
 				return res.status(404).json({ message: 'No user found with this id!' })
 			}
-			res.json(user)
+			return res.status(200).json(user)
 		} catch (err) {
 			console.error(err)
 			res.status(400).json(err)
@@ -40,11 +40,15 @@ const userController = {
 	// Update user by its _id
 	async updateUser(req, res) {
 		try {
-			const user = await User.findOneAndUpdate({ _id: req.params.id }, req.body, { new: true })
+			const user = await User.findOneAndUpdate(
+				{ _id: req.params.userId },
+				req.body,
+				{ runValidators: true, new: true }
+				)
 			if (!user) {
 				return res.status(404).json({ message: 'No user found with this id!' })
 			}
-			res.json(user)
+			return res.status(200).json(user)
 		} catch (err) {
 			console.error(err)
 			return res.status(400).json(err)
@@ -54,12 +58,30 @@ const userController = {
 	// DELETE user by its _id
 	async deleteUser(req, res) {
 		try {
-			const user = await User.findOneAndDelete({ _id: req.params.id })
+			const user = await User.findOneAndDelete({ _id: req.params.userId })
 			if (!user) {
 				return res.status(404).json({ message: 'No user found with this id!' })
 			}
 			// TODO: Delete associated thoughts when user is deleted.
 			res.status(200).json({ message: 'User deleted!' })
+		} catch (err) {
+			console.error(err)
+			return res.status(400).json(err)
+		}
+	},
+
+	// Add friend to user's friend list
+	async addFriend(req, res) {
+		try {
+			const friend = await User.findOneAndUpdate(
+				{ _id: req.params.userId },
+				{ friends: req.params.friendId },
+				{ runValidators: true, new: true }
+			)
+			if (!friend) {
+				return res.status(404).json({ message: 'No user found with this id!' })
+			}
+			return res.status(200).json(friend)
 		} catch (err) {
 			console.error(err)
 			return res.status(400).json(err)
--- routes/api/userRoutes.js
+++ routes/api/userRoutes.js
@@ -1,8 +1,10 @@
 const router = require('express').Router()
-const { getUsers, createUser, getUserById, updateUser, deleteUser } = require('../../controllers/userController')
+const { getUsers, createUser, getUserById, updateUser, deleteUser, addFriend } = require('../../controllers/userController')
 
 router.route('/').get(getUsers).post(createUser)
 
-router.route('/:id').get(getUserById).put(updateUser).delete(deleteUser)
+router.route('/:userId').get(getUserById).put(updateUser).delete(deleteUser)
+
+router.route('/:userId/friends/:friendId').post(addFriend)
 
 module.exports = router
